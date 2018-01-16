cfa3bb593f774ede7eb625d5aadb4a8187bac3ab
ImpactMediaLab <tuff.kika@gmail.com>
2018-01-15 19:36:42 -0600

    kika first design changes

--- server.R
+++ server.R
@@ -1,9 +1,11 @@
+# Template for BIOMAAP educational modules
+# January 15th, 2018
+# Created by Impact Media Lab
+
+######### Load required packages
+
 library(shiny)
 library(shinydashboard)
-library(scales)
-#library(MASS)
-#data(beav1)
-
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
@@ -13,18 +15,69 @@ shinyServer(function(input, output, session) {
   output$plot1 <- renderPlot({
     data <- histdata[seq_len(input$slider)]
     hist(data)
+    
+    
   
      })
+  
+  ######### Info boxes used in the module
+  
+  output$progressBox <- renderInfoBox({
+    infoBox(
+      "Progress", paste0(25 + input$count, "%"), icon = icon("list"),
+      color = "purple"
+    )
+  })
+  output$approvalBox <- renderInfoBox({
+    infoBox(
+      "Approval", "80%", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "yellow"
+    )
+  })
+  
+  # Same as above, but with fill=TRUE
+  output$progressBox2 <- renderInfoBox({
+    infoBox(
+      "Progress", paste0(25 + input$count, "%"), icon = icon("list"),
+      color = "purple", fill = TRUE
+    )
+  })
+  output$approvalBox2 <- renderInfoBox({
+    infoBox(
+      "Approval", "80%", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "yellow", fill = TRUE
+    )
+  })
     
-    
-  output$experimental_design <- renderImage({
+  ######### Images used in the module
+  
+  output$BIOMAAPlogo_photo <- renderImage({
     list(
-      src = "www/Napoleon.png",
+      src = "www/BioMAAP_logo2.png",
       contentType = "image/png",
       alt = "Face"
     )}, deleteFile = FALSE)
   
+  output$survey_photo <- renderImage({
+    list(
+      src = "www/survey.png",
+      contentType = "image/png",
+      alt = "Face"
+    )}, deleteFile = FALSE)
   
+  output$results_photo <- renderImage({
+    list(
+      src = "www/bar chart.png",
+      contentType = "image/png",
+      alt = "Face"
+    )}, deleteFile = FALSE)
+  
+  output$math_anxiety_photo <- renderImage({
+    list(
+      src = "www/math_anxiety.png",
+      contentType = "image/png",
+      alt = "Face"
+    )}, deleteFile = FALSE)
   
   output$Big_Data_Traffic <- renderImage({
     list(
@@ -32,5 +85,13 @@ shinyServer(function(input, output, session) {
       contentType = "image/png",
       alt = "Face"
     )}, deleteFile = FALSE)
+  
+  output$looking_hard <- renderImage({
+    list(
+      src = "www/looking_hard_at_data.png",
+      contentType = "image/png",
+      alt = "Face"
+    )}, deleteFile = FALSE)
 })
+  
 
--- ui.R
+++ ui.R
@@ -1,55 +1,88 @@
+# Template for BIOMAAP educational modules
+# January 15th, 2018
+# Created by Impact Media Lab
 
-   
+######### Load required packages
+
 library(shiny)
 library(shinydashboard)
-library(scales)
-#library(MASS)
-#data(beav1)
 
-library(shinydashboard)
+######### A dashboard has three parts: a header, a sidebar, and a body. 
+shinyUI(dashboardPage(skin ="black",
+  
+# HEADER  
+  dashboardHeader(title = "BIOMAAP module template",
+                  
+                  
+                  ##### DROPDOWN MENUS
+                  
+                  # DROPDOWN MENU: TASKS
+                  dropdownMenu(type = "tasks", badgeStatus = "success",
+                               taskItem(value = 90, color = "green",
+                                        "Documentation"
+                               ),
+                               taskItem(value = 17, color = "aqua",
+                                        "Project X"
+                               ),
+                               taskItem(value = 75, color = "yellow",
+                                        "Server deployment"
+                               ),
+                               taskItem(value = 80, color = "red",
+                                        "Overall project"
+                               )
+                  )),
 
-
-
-shinyUI(dashboardPage(
-  dashboardHeader(title = "Testing the value of a growth mindset", titleWidth = 400),
+# SIDEBAR  
   dashboardSidebar(
     sidebarMenu(
-      menuItem("intro", tabName = "intro", icon = icon("dashboard")),
-      menuItem("exam", tabName = "exam", icon = icon("dashboard")),
-      menuItem("lesson", tabName = "lesson", icon = icon("dashboard")),
-      menuItem("results", tabName = "results", icon = icon("th"))
+      menuItem("Welcome", tabName = "welcome", icon = icon("hand-spock-o")),
+      menuItem("Survey", tabName = "survey", icon = icon("pencil-square")),
+      menuItem("Lesson", tabName = "lesson", icon = icon("graduation-cap")),
+      menuItem("Quiz", tabName = "quiz", icon = icon("question-circle")),
+      menuItem("Results", tabName = "results", icon = icon("bar-chart"))
     )
   ),
+
+#BODY
+# Boxes need to be put in a row (or column)  
   dashboardBody(
     tabItems(
-    tabItem(tabName = "intro",
+      
+      # WELCOME PAGE
+      tabItem(tabName = "welcome",
              fluidRow(
-               imageOutput("experimental_design", height = 430, width = 400)
+               img(src ="BioMAAP_logo2.png", width = 450, style="display: block; margin-left: auto; 
+                   margin-right: auto; margin-top:40px; margin-bottom:20px")
+               #imageOutput("BIOMAAPlogo_photo", height = "100px", width = "100px", inline = T)
              ),
-             p("",span(tags$a(href = "http://benfry.com/projects", "Check out Ben Fry's gallary", style = "color:navy")), align = "right"),
+             #p("",span(tags$a(href = "http://benfry.com/projects", "Check out Ben Fry's gallary", style = "color:navy")), align = "right"),
              
-             tags$h1("Using quantitative thought to analyze data"),
-             tags$h4(p("Created by", span(tags$a(href = "http://www.colorado.edu/eeb/tuff/Tuff_et_al./Ty_Tuff.html", "Ty Tuff", style = "color:black")), "and Kika Tuff"  ), align="center"),
+             tags$h2("SKILLS TO EMBRACE MATH AND SUCCEED IN BIOLOGY", align = "center"),
+             tags$h4(p("Developed by Arietta Fleming-Davies and Jeremy Wojdak", style = "color:black", align="center")),
+             tags$h4(p("Built by", span(tags$a(href = "https://www.impactmedialab.com/", "Impact Media Lab", style = "color:black"))), align="center"),
              ### tags$iframe(id="iframe1", src="http://player.vimeo.com/video/21172499?"),
+            
+             br(),
              
-            p("The purpose of this lab is to introduce you to statistical analysis tools (in this case R) and to give you a framework that will help you consider data and data analysis based on the process of quantitative thinking. This will become important both as you move forward with your independent projects and in your future academic and professional careers. This shinyapps (https://www.shinyapps.io) online workflow application will help facilitate the learning process without the distraction of having to learn how to code.")
+             p("The Biology Students Math Attitudes and Anxiety Program is a resource for 
+              educators who are looking to implement non-invasive techniques to change 
+              student attitudes and reduce anxiety towards math. Lorem ipsum dolor sit amet, 
+               consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+               laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+               reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+               deserunt mollit anim id est laborum"),
+            
+            fluidRow(
+              img(src ="line.png", width = "100%", style="display: block; margin-left: auto; 
+                   margin-right: auto")
+            )
       
       ),
-      tabItem(tabName = "exam",
-               fluidRow(
-                 imageOutput("Big_Data_Traffic", height = 430, width = 400)
-               ),
-               p("",span(tags$a(href = "http://benfry.com/projects", "Check out Ben Fry's gallary", style = "color:navy")), align = "right"),
-               
-               tags$h1("Using quantitative thought to analyze data"),
-               tags$h4(p("Created by", span(tags$a(href = "http://www.colorado.edu/eeb/tuff/Tuff_et_al./Ty_Tuff.html", "Ty Tuff", style = "color:black")), "and Kika Tuff"  ), align="center"),
-               ### tags$iframe(id="iframe1", src="http://player.vimeo.com/video/21172499?"),
-               
-               p("The purpose of this lab is to introduce you to statistical analysis tools (in this case R) and to give you a framework that will help you consider data and data analysis based on the process of quantitative thinking. This will become important both as you move forward with your independent projects and in your future academic and professional careers. This shinyapps (https://www.shinyapps.io) online workflow application will help facilitate the learning process without the distraction of having to learn how to code.")
-               
-        ),
-            # First tab content
-      tabItem(tabName = "lesson",
+      
+      # STUDENT SURVEY PAGE
+      tabItem(tabName = "survey",
               fluidRow(
                 box(plotOutput("plot1", height = 250)),
                 
@@ -60,11 +93,48 @@ shinyUI(dashboardPage(
               )
       ),
       
-      # Second tab content
+      # LESSON ON TOPIC PAGE
+      tabItem(tabName = "lesson",
+              fluidRow(
+                imageOutput("looking_hard", height = 430, width = 400)
+              )
+              ),
+      
+      # QUIZ ON TOPIC PAGE
+      tabItem(tabName = "quiz",
+              fluidRow(
+                #box(plotOutput("plot1", height = 250)),
+                
+                box(
+                  title = "Controls",
+                  sliderInput("slider", "Number of observations:", 1, 100, 50)
+                )
+              )
+      ),
+      
+      # RESULTS PAGE
       tabItem(tabName = "results",
-              h2("Widgets tab content")
+              # infoBoxes with fill=FALSE
+              fluidRow(
+                  # A static infoBox
+                  infoBox("New Orders", 10 * 2, icon = icon("credit-card")),
+                  # Dynamic infoBoxes
+                  infoBoxOutput("progressBox"),
+                  infoBoxOutput("approvalBox")
+              ),
+                
+              # infoBoxes with fill=TRUE
+              fluidRow(
+                  infoBox("New Orders", 10 * 2, icon = icon("credit-card"), fill = TRUE),
+                  infoBoxOutput("progressBox2"),
+                  infoBoxOutput("approvalBox2")
+              ),
+                
+              fluidRow(
+                # Clicking this will increment the progress amount
+                  box(width = 4, actionButton("count", "Increment progress"))
+              )
+       )
       )
-    ))
-  ))
-  
-  
+  )
+))
